c44f1f8078c8400ce672de3ff66d6c054e3dc138
Natasja Eibrink <natasjaeibrink@gmail.com>
2018-04-03 09:20:38 +0200

    first changes

---
+++ src/main/java/nl/yacht/XYZAirlines/XyzAirlinesApplication.java
@@ -0,0 +1,12 @@
+package nl.yacht.XYZAirlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzAirlinesApplication.class, args);
+	}
+}
